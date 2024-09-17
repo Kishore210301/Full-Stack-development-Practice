a071528565d73e88d5bbc07ccfc521d2a7db8152
Kishore210301 <kishore.20tt@kct.ac.in>
2024-09-17 10:28:21 +0530

    All Practice Files Added

---
+++ Java Projects/Git hub Kishore/new.java
@@ -0,0 +1,8 @@
+public class New{
+    public static void main(String[] args){
+        int height = 10;
+        for(i=0 , i<height , i++){
+            System.out.println("+");
+        }
+    }
+}
